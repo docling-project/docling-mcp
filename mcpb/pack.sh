a63b89e3cedaa41d6e418edfc49c47e6dd6f80b9
Michele Dolfi <dol@zurich.ibm.com>
2025-09-23 10:31:12 +0200

    update version automatically
    Signed-off-by: Michele Dolfi <dol@zurich.ibm.com>

--- mcpb/pack.sh
+++ mcpb/pack.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+set -e
+
+# Read version from ../pyproject.toml
+VERSION=$(grep -E '^[[:space:]]*version[[:space:]]*=' ../pyproject.toml \
+          | head -n1 \
+          | sed -E 's/[[:space:]]*version[[:space:]]*=[[:space:]]*"([^"]+)".*/\1/')
+echo "Using version: $VERSION"
+
+# Update manifest.json
+jq --arg ver "$VERSION" \
+   '.version = $ver | .user_config.version.default = $ver' \
+   manifest.json > manifest.tmp.json && mv manifest.tmp.json manifest.json
 
 # Python lib
 python -m pip install -U pipx --target ./lib
